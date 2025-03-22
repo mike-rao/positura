9748b9e83b29796d147f771af84020f812fe6cea
Juwanz <haozewang1279@gmail.com>
2025-03-22 16:11:55 -0500

    csv creation

--- backend/posture_detection.py
+++ backend/posture_detection.py
@@ -1,7 +1,9 @@
 import cv2
 import mediapipe as mp
 import numpy as np
+import csv
 
+filename = 'data.csv'
 # Initialize MediaPipe Pose
 mp_pose = mp.solutions.pose
 mp_drawing = mp.solutions.drawing_utils
@@ -56,6 +58,18 @@ while cap.isOpened():
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     angle = None  # Default to None in case no detection happens
+
+    #write to csv
+    data = []
+    for i in range(len(landmarks)):
+        data.append(str(landmarks[i].x) + ', ' + str(landmarks[i].y) + ", ")
+    #make csv
+    with open(filename, 'a', newline='') as file:
+        # Create a CSV writer object
+        writer = csv.writer(file)
+
+        # Write the data rows to the CSV file
+        writer.writerows(data)
 
     if results.pose_landmarks:
         landmarks = results.pose_landmarks.landmark
